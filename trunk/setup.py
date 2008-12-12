ab9ef9a332e7e32d02c7cc2861ab0f6e2c05d04c
Bart Whiteley <bwhiteley@gmail.com>
2008-12-12 17:22:03 +0000

    change version to 0.7.0
    git-svn-id: https://svn.code.sf.net/p/pywbem/code/pywbem@545 84f79d66-d411-0410-a85a-a3183736c6e8

--- trunk/setup.py
+++ trunk/setup.py
@@ -35,7 +35,7 @@ args = {'name': 'pywbem',
         'long_description': __doc__,
         'platforms': ['any'],
         'url': 'http://pywbem.sf.net/',
-        'version': '0.7.0rc1',
+        'version': '0.7.0',
         'license': 'LGPLv2',
         'packages': ['pywbem'],
         # Make packages in root dir appear in pywbem module
