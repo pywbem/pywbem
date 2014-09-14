5a2f22cee21374c3339c50299ff2e391fa95f7bf
Andreas Maier <maiera@de.ibm.com>
2014-09-14 20:16:53 +0000

    Improved package documentation in pywbem/__init__.py. Improved information in README file.
    git-svn-id: https://svn.code.sf.net/p/pywbem/code/pywbem@649 84f79d66-d411-0410-a85a-a3183736c6e8

--- trunk/pywbem/__init__.py
+++ trunk/pywbem/__init__.py
@@ -1,5 +1,3 @@
-# Package init file for pywbem
-
 #
 # (C) Copyright 2004,2006 Hewlett-Packard Development Company, L.P.
 #
@@ -20,15 +18,116 @@
 # Author: Tim Potter <tpot@hp.com>
 #         Martin Pool <mbp@hp.com>
 
-"""pywbem - WBEM client bindings for Python.
+"""
+PyWBEM provides a WBEM client library and some related utilities, written in
+pure Python.
 
-PyWBEM is a Python library for making CIM operations over HTTP using
-the CIM-XML protocol.  It is based on the idea that a good WBEM
-client should be easy to use and not necessarily require a large
-amount of programming knowlege.  PyWBEM is suitable for a large range
-of tasks from simply poking around to writing web and GUI
+The WBEM client library allows issuing operations to a WBEM server, using
+the CIM operations over HTTP (CIM-XML) protocol defined in the DMTF standards
+DSP0200 and DSP0201. See http://www.dmtf.org/standards/wbem for information
+about WBEM.
+
+It is based on the idea that a good WBEM client should be easy to use and not
+necessarily require a large amount of programming knowledge. It is suitable for
+a large range of tasks from simply poking around to writing web and GUI
 applications.
 
+  * `pywbem.cim_operations.WBEMConnection` :  Main class of the WBEM client
+    library and a good starting point to read about it.
+
+The WBEM-related utilities included in this package are:
+
+  * `pywbem.mof_compiler` :   Script for compiling MOF files, can also be used
+    as a module.
+
+  * `pywbem.cim_provider` :   Module for writing CIM providers in Python.
+
+  * `pywbem.cim_provider2` :  Another module for writing CIM providers in
+    Python.
+
+  * `pywbem.twisted_client` : An experimental alternative WBEM client library
+    that uses the Python `twisted` package.
+
+  * [`wbemcli.py`](../wbemcli.m.html) :  Script providing a WBEM client CLI
+    as an interactive shell.
+
+Importing the `pywbem` package causes a subset of symbols from its sub-modules
+to be folded into the target namespace.
+
+The use of these folded symbols is shown for the example of class
+`pywbem.cim_operations.WBEMConnection`:
+
+    import pywbem
+    conn = pywbem.WBEMConnection(...)
+
+or:
+
+    from pywbem import WBEMConnection
+    conn = WBEMConnection(...)
+
+or (less preferred):
+
+    from pywbem import *
+    conn = WBEMConnection(...)
+
+The folded symbols' origin symbols in the sub-modules are also considered part
+of the public interface of the `pywbem` package.
+
+Programs using sub-modules that are not part of the WBEM client library, or
+specific symbols that are not folded into the target namespace of the `pywbem`
+package need to import the respective sub-modules explicitly.
+
+The use of such sub-modules is shown for the example of class
+`pywbem.cim_provider.CIMProvider`:
+
+    from pywbem import cim_provider
+    provider = cim_provider.CIMProvider(...)
+
+or:
+
+    from pywbem.cim_provider import CIMProvider
+    provider = CIMProvider(...)
+
+or:
+
+    import pywbem.cim_provider
+    provider = pywbem.cim_provider.CIMProvider(...)
+
+Version
+-------
+
+This version of PyWBEM is 0.8.0~dev.
+
+The version number follows these conventions:
+
+  * M.N.U~dev : During development of the future M.N.U version.
+  * M.N.U~rc1 : For release candidate 1 (etc.) of the future M.N.U version.
+  * M.N.U     : For the final (=released) M.N.U version.
+
+The use of the tilde (~) causes RPM to correctly treat the preliminary versions
+to be younger than the final version.
+
+Changes
+-------
+
+The change log is in the NEWS file of the PyWBEM distribution archive.
+
+Compatibility
+-------------
+
+PyWBEM has been tested with Python 2.6 on Windows and Linux, and with Python
+2.7 on Linux (due to a restriction of the `M2Crypto` package on Windows).
+
+Contributing
+------------
+
+PyWBEM [is on SourceForge](http://sourceforge.net/projects/pywbem/). Bug
+reports and discussion on the mailing list are welcome.
+
+License
+-------
+
+PyWBEM is licensed with GNU LGPL v2.
 """
 
 # Version of the pywbem package
@@ -44,4 +143,3 @@ from pywbem.cim_constants import *
 from pywbem.cim_operations import *
 from pywbem.cim_obj import *
 from pywbem.tupleparse import ParseError
-
