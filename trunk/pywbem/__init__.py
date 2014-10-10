d63e86daa1320e48a56dc3a398cfcd3a50ce3e3b
Andreas Maier <maiera@de.ibm.com>
2014-10-10 18:12:02 +0000

    Improved documentation in pywbem/__init__.py.
    git-svn-id: https://svn.code.sf.net/p/pywbem/code/pywbem@672 84f79d66-d411-0410-a85a-a3183736c6e8

--- trunk/pywbem/__init__.py
+++ trunk/pywbem/__init__.py
@@ -32,30 +32,28 @@ necessarily require a large amount of programming knowledge. It is suitable for
 a large range of tasks from simply poking around to writing web and GUI
 applications.
 
-  * `pywbem.cim_operations.WBEMConnection` :  Main class of the WBEM client
-    library and a good starting point to read about it.
+* `WBEMConnection` :  Main class of the WBEM client library and a good starting
+  point to read about it.
 
 The WBEM-related utilities included in this package are:
 
-  * `pywbem.mof_compiler` :   Script for compiling MOF files, can also be used
-    as a module.
+* `mof_compiler` :   Script for compiling MOF files, can also be used as a
+  module.
 
-  * `pywbem.cim_provider` :   Module for writing CIM providers in Python.
+* `cim_provider` :   Module for writing CIM providers in Python.
 
-  * `pywbem.cim_provider2` :  Another module for writing CIM providers in
-    Python.
+* `cim_provider2` :  Another module for writing CIM providers in Python.
 
-  * `pywbem.twisted_client` : An experimental alternative WBEM client library
-    that uses the Python `twisted` package.
+* `twisted_client` : An experimental alternative WBEM client library that uses
+  the Python `twisted` package.
 
-  * `pywbem.wbemcli` :  Script providing a WBEM client CLI
-    as an interactive shell.
+* `wbemcli` :  Script providing a WBEM client CLI as an interactive shell.
 
 Importing the `pywbem` package causes a subset of symbols from its sub-modules
 to be folded into the target namespace.
 
 The use of these folded symbols is shown for the example of class
-`pywbem.cim_operations.WBEMConnection`:
+`WBEMConnection`:
 
 .. code:: python
 
@@ -84,7 +82,7 @@ specific symbols that are not folded into the target namespace of the `pywbem`
 package need to import the respective sub-modules explicitly.
 
 The use of such sub-modules is shown for the example of class
-`pywbem.cim_provider.CIMProvider`:
+`cim_provider.CIMProvider`:
 
 .. code:: python
 
@@ -112,9 +110,9 @@ This version of PyWBEM is 0.8.0~dev.
 
 The version number follows these conventions:
 
-  * M.N.U~dev : During development of the future M.N.U version.
-  * M.N.U~rc1 : For release candidate 1 (etc.) of the future M.N.U version.
-  * M.N.U     : For the final (=released) M.N.U version.
+* M.N.U~dev : During development of the future M.N.U version.
+* M.N.U~rc1 : For release candidate 1 (etc.) of the future M.N.U version.
+* M.N.U     : For the final (=released) M.N.U version.
 
 The use of the tilde (~) causes RPM to correctly treat the preliminary versions
 to be younger than the final version.
@@ -122,27 +120,29 @@ to be younger than the final version.
 Changes
 -------
 
-The change log is in the NEWS file of the PyWBEM distribution archive.
+The change log is in the `NEWS <../NEWS>`_ file.
 
 Compatibility
 -------------
 
-PyWBEM has been tested with Python 2.6 on Windows and Linux, and with Python
-2.7 on Linux (due to a restriction of the `M2Crypto` package on Windows).
+PyWBEM has been tested with Python 2.7 on Windows and Linux, and with Python
+2.6 on Linux (due to a restriction of the `M2Crypto` package on Windows).
 
 Contributing
 ------------
 
-PyWBEM [is on SourceForge](http://sourceforge.net/projects/pywbem/). Bug
+PyWBEM is on SourceForge (http://sourceforge.net/projects/pywbem/). Bug
 reports and discussion on the mailing list are welcome.
 
 License
 -------
 
 PyWBEM is licensed with GNU LGPL v2.
+See the `LICENSE.txt <../LICENSE.txt>`_ file.
 """
 
 # Version of the pywbem package
+# !!! Keep in sync with version stated in module docstring, above !!!
 __version__ = '0.8.0~dev'
 
 # There are submodules, but clients shouldn't need to know about them.
