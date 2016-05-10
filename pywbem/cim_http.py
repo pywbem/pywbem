8013af7b6ba812fb36e87cb224d9bf417699d705
kschopmeyer <k.schopmeyer@swbell.net>
2016-05-10 18:09:30 -0500

    Fixes a couple of issues. 1) the wrap function needs the hostname, 2) message misspelling, 3) insure check_hostname state

--- pywbem/cim_http.py
+++ pywbem/cim_http.py
@@ -511,6 +511,7 @@ def wbem_request(url, data, creds, headers=[], debug=0, x509=None,
                         ctx.load_verify_locations(cafile=self.ca_certs)
                     ctx.check_hostname = True
                 else:
+                    ctx.check_hostname = False
                     ctx.verify_mode = SSL.CERT_NONE
 
                 # setup the socket
@@ -518,7 +519,8 @@ def wbem_request(url, data, creds, headers=[], debug=0, x509=None,
                 sock.settimeout(self.timeout)
 
                 try:
-                    self.sock = ctx.wrap_socket(sock)
+                    self.sock = ctx.wrap_socket(sock,
+                                                server_hostname=self.host)
                     return self.sock.connect((self.host, self.port))
 
                 except SSLError as arg:
@@ -526,7 +528,7 @@ def wbem_request(url, data, creds, headers=[], debug=0, x509=None,
                         "SSL error %s: %s" % (arg.__class__, arg))
                 except CertificateError as arg:
                     raise ConnectionError(
-                        "SSL Certificateerror %s: %s" % (arg.__class__, arg))
+                        "SSL certificate error %s: %s" % (arg.__class__, arg))
 
     class FileHTTPConnection(HTTPBaseConnection, httplib.HTTPConnection):
         """Execute client connection based on a unix domain socket. """
