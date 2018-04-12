641d4ff9a6ffe1e01a3a9a267db4ba2879092e0d
Andreas Maier <maiera@de.ibm.com>
2018-04-12 23:38:05 +0200

    Added support to ValueMapping for translating to binary and for iterating
    Details:
    
    - Added a `tobinary()` method to the `ValueMapping` class, which translates the
      value mapping from a `Values` string to binary integer values, or a range
      thereof. This is the opposite direction of the existing `tovalues()` method.
    - Added an `items()` generator method for iterating through the items
      of the mapping, returning tuples of the binary value (or a range thereof), and
      the `Values` string.
    - Extended the testcases accordingly.
    
    Signed-off-by: Andreas Maier <maiera@de.ibm.com>

--- pywbem/_valuemapping.py
+++ pywbem/_valuemapping.py
@@ -15,10 +15,10 @@
 #
 
 """
-The :class:`~pywbem.ValueMapping` class supports translating the values of an
-integer-typed CIM element (e.g. property, method, or parameter) that is
-qualified with the `ValueMap` and `Values` qualifiers, to the corresponding
-value of the `Values` qualifier.
+The :class:`~pywbem.ValueMapping` class supports translating between the values
+of an integer-typed CIM element (e.g. property, method, or parameter) that is
+qualified with the `ValueMap` and `Values` qualifiers, and the corresponding
+values of the `Values` qualifier, in both directions.
 
 This class supports value ranges (e.g. ``"4..6"``) and the unclaimed marker
 (``".."``).
@@ -26,6 +26,10 @@ This class supports value ranges (e.g. ``"4..6"``) and the unclaimed marker
 
 import re
 import six
+try:
+    from collections import OrderedDict
+except ImportError:
+    from ordereddict import OrderedDict
 
 from .cim_types import CIMInt, type_from_name
 
@@ -36,17 +40,18 @@ class ValueMapping(object):
     """
     *New in pywbem 0.9 as experimental and finalized in 0.10.*
 
-    A utility class that supports translating the values of an integer-typed
-    CIM element (property, method, parameter) that is qualified with the
-    `ValueMap` and `Values` qualifiers, to the corresponding value of the
-    `Values` qualifier.
+    A utility class that supports translating between the values of an
+    integer-typed CIM element (property, method, parameter) that is qualified
+    with the `ValueMap` and `Values` qualifiers, and the corresponding values
+    of the `Values` qualifier, in both directions.
 
     This is done by retrieving the CIM class definition defining the CIM
     element in question, and by inspecting its `ValueMap` and `Values`
     qualifiers.
 
-    The translation of the values is performed by the
-    :meth:`~pywbem.ValueMapping.tovalues` method.
+    The translation is performed by the
+    :meth:`~pywbem.ValueMapping.tovalues` and
+    :meth:`~pywbem.ValueMapping.tobinary` methods.
 
     Instances of this class must be created through one of the factory class
     methods: :meth:`~pywbem.ValueMapping.for_property`,
@@ -72,8 +77,10 @@ class ValueMapping(object):
           };
 
       Assuming this class exists in a WBEM server, the following code will
-      create a value mapping for this property and will print a few property
-      values and their corresponding `Values` strings::
+      get the class from the server, create a value mapping for this property,
+      and look up the `Values` strings that correspond to binary property
+      values. This is useful when preparing binary property values for human
+      consumption::
 
           namespace = 'root/cimv2'
           conn = pywbem.WBEMConnection(...)  # WBEM server
@@ -81,27 +88,92 @@ class ValueMapping(object):
           myprop_vm = pywbem.ValueMapping.for_property(
               conn, namespace, 'CIM_Foo', 'MyProp')
 
-          print("value: Values")
-          for value in range(0, 12):
-              print("%5s: %r" % (value, myprop_vm.tovalues(value))
+          print("Binary value: Values string")
+          for bin_value in range(0, 12):
+              values_str = myprop_vm.tovalues(bin_value)
+              print("%12s: %r" % (bin_value, values_str))
 
       Resulting output:
 
       .. code-block:: text
 
-          value: Values
-              0: 'zero'
-              1: 'unclaimed'
-              2: 'two-four'
-              3: 'two-four'
-              4: 'two-four'
-              5: 'five-six'
-              6: 'five-six'
-              7: 'seven-eight'
-              8: 'seven-eight'
-              9: 'nine'
-             10: 'unclaimed'
-             11: 'unclaimed'
+          Binary value: Values string
+                     0: 'zero'
+                     1: 'unclaimed'
+                     2: 'two-four'
+                     3: 'two-four'
+                     4: 'two-four'
+                     5: 'five-six'
+                     6: 'five-six'
+                     7: 'seven-eight'
+                     8: 'seven-eight'
+                     9: 'nine'
+                    10: 'unclaimed'
+                    11: 'unclaimed'
+
+      Translating in the other direction is also of interest, for example when
+      processing values that are provided by humans in terms of the `Values`
+      strings, or when using the pywbem mock support and the test cases specify
+      property values for CIM instances in terms of the more human-readable
+      `Values` strings.
+
+      Again, assuming the class shown above exists in a WBEM server, the
+      following code will get the class from the server, create a value
+      mapping for this property, and look up the binary property values from
+      the `Values` strings::
+
+          namespace = 'root/cimv2'
+          conn = pywbem.WBEMConnection(...)  # WBEM server
+
+          myprop_vm = pywbem.ValueMapping.for_property(
+              conn, namespace, 'CIM_Foo', 'MyProp')
+
+          values_strs = ["zero", "two-four", "five-six", "seven-eight", "nine",
+                         "unclaimed"]
+
+          print("Values string: Binary value")
+          for values_str in values_strs:
+              bin_value = myprop_vm.tobinary(values_str)
+              print("%12s: %r" % (values_str, bin_value))
+
+      Resulting output:
+
+      .. code-block:: text
+
+          Values string: Binary value
+                 'zero': 0
+             'two-four': (2, 4)
+             'five-six': (5, 6)
+          'seven-eight': (7, 8)
+                 'nine': 9
+            'unclaimed': None
+
+      Iterating through the pairs of `ValueMap` and `Values` entries is
+      also possible. Assuming the class shown above exists in a WBEM server, the
+      following code will get the class from the server, and iterate through the
+      value mapping::
+
+          namespace = 'root/cimv2'
+          conn = pywbem.WBEMConnection(...)  # WBEM server
+
+          myprop_vm = pywbem.ValueMapping.for_property(
+              conn, namespace, 'CIM_Foo', 'MyProp')
+
+          print("Values string: Binary value")
+          for bin_value, values_str in myprop_vm.items():
+              print("%12s: %r" % (values_str, bin_value))
+
+      Resulting output:
+
+      .. code-block:: text
+
+          Values string: Binary value
+                 'zero': 0
+             'two-four': (2, 4)
+             'five-six': (5, 6)
+          'seven-eight': (7, 8)
+                 'nine': 9
+            'unclaimed': None
     """
 
     def __init__(self):
@@ -115,9 +187,13 @@ class ValueMapping(object):
 
         self._element_obj = None
 
-        self._single_dict = {}  # for single values; elem_val: values_str)
-        self._range_tuple_list = []  # for value ranges; tuple(lo,hi,values_str)
-        self._unclaimed = None  # value of the unclaimed indicator '..'
+        # Attributes for converting binary values to Values strings:
+        self._b2v_single_dict = {}  # for single values; bin: values
+        self._b2v_range_tuple_list = []  # for value ranges; tuple(lo,hi,values)
+        self._b2v_unclaimed = None  # value of the unclaimed indicator '..'
+
+        # Attributes for converting Values strings to binary values:
+        self._v2b_dict = {}  # values: bin (int or tuple)
 
     @classmethod
     def for_property(cls, server, namespace, classname, propname):
@@ -304,7 +380,8 @@ class ValueMapping(object):
     @classmethod
     def _values_tuple(cls, i, valuemap_list, values_list, cimtype):
         """
-        Return a tuple for the value range at position i, with these items:
+        Return a tuple for the value range or unclaimed marker at position i,
+        with these items:
 
         * lo - low value of the range
         * hi - high value of the range (can be equal to lo)
@@ -436,27 +513,34 @@ class ValueMapping(object):
         valuemap_qual = element_obj.qualifiers.get('ValueMap', None)
         if valuemap_qual is None:
             # DSP0004 defines a default of consecutive index numbers
-            vm._single_dict = dict(zip(range(0, len(values_list)), values_list))
-            vm._range_tuple_list = []
-            vm._unclaimed = None
+            vm._b2v_single_dict = dict(zip(range(0, len(values_list)),
+                                           values_list))
+            vm._b2v_range_tuple_list = []
+            vm._b2v_unclaimed = None
+            vm._v2b_dict = OrderedDict(zip(values_list,
+                                           range(0, len(values_list))))
         else:
-            vm._single_dict = {}
-            vm._range_tuple_list = []
-            vm._unclaimed = None
+            vm._b2v_single_dict = {}
+            vm._b2v_range_tuple_list = []
+            vm._b2v_unclaimed = None
+            vm._v2b_dict = OrderedDict()
             valuemap_list = valuemap_qual.value
             for i, valuemap_str in enumerate(valuemap_list):
                 values_str = values_list[i]
                 if valuemap_str == '..':
-                    vm._unclaimed = values_str
+                    vm._b2v_unclaimed = values_str
+                    vm._v2b_dict[values_str] = None
                 else:
                     lo, hi, values_str = cls._values_tuple(
                         i, valuemap_list, values_list, cimtype)
                     if lo == hi:
                         # single value
-                        vm._single_dict[lo] = values_str
+                        vm._b2v_single_dict[lo] = values_str
+                        vm._v2b_dict[values_str] = lo
                     else:
                         # value range
-                        vm._range_tuple_list.append((lo, hi, values_str))
+                        vm._b2v_range_tuple_list.append((lo, hi, values_str))
+                        vm._v2b_dict[values_str] = (lo, hi)
 
         # pylint: enable=protected-access
         return vm
@@ -469,11 +553,13 @@ class ValueMapping(object):
         return "%s(_conn=%r, _namespace=%r, " \
                "_classname=%r, _propname=%r, _methodname=%r, " \
                "_parametername=%r, _element_obj=%r, " \
-               "_single_dict=%r, _range_tuple_list=%r, _unclaimed=%r)" % \
+               "_b2v_single_dict=%r, _b2v_range_tuple_list=%r, " \
+               "_b2v_unclaimed=%r, _v2b_dict=%r)" % \
                (self.__class__.__name__, self._conn, self._namespace,
                 self._classname, self._propname, self._methodname,
                 self._parametername, self._element_obj,
-                self._single_dict, self._range_tuple_list, self._unclaimed)
+                self._b2v_single_dict, self._b2v_range_tuple_list,
+                self._b2v_unclaimed, self._v2b_dict)
 
     @property
     def conn(self):
@@ -560,19 +646,99 @@ class ValueMapping(object):
 
         # try single value
         try:
-            return self._single_dict[element_value]
+            return self._b2v_single_dict[element_value]
         except KeyError:
             pass
 
         # try value ranges
-        for range_tuple in self._range_tuple_list:
+        for range_tuple in self._b2v_range_tuple_list:
             lo, hi, values_str = range_tuple
             if lo <= element_value <= hi:
                 return values_str
 
         # try catch-all '..'
-        if self._unclaimed is not None:
-            return self._unclaimed
+        if self._b2v_unclaimed is not None:
+            return self._b2v_unclaimed
 
         raise ValueError("Element value outside of the set defined by "
                          "ValueMap: %r" % element_value)
+
+    def tobinary(self, values_str):
+        """
+        Return the integer value or values for a `Values` string, based upon
+        this value mapping.
+
+        Any returned integer value is represented as the CIM type of the
+        element (e.g. :class:`~pywbem.Uint16`).
+
+        If the `Values` string corresponds to a single value, that single value
+        is returned as an integer.
+
+        If the `Values` string corresponds to a value range (e.g. "1.." or
+        "..2" or "3..4"), that value range is returned as a tuple with two items
+        that are the lowest and the highest value of the range. That is the
+        case also when the value range is open on the left side or right side.
+
+        If the `Values` string corresponds to the `unclaimed` indicator "..",
+        `None` is returned.
+
+        Parameters:
+
+          values_str (:term:`string`):
+            The `Values` string for the element value.
+
+        Returns:
+
+          :class:`~pywbem.CIMInt` or tuple of :class:`~pywbem.CIMInt` or `None`:
+            The element value or value range corresponding to the `Values`
+            string, or `None` for unclaimed.
+
+        Raises:
+
+          ValueError: `Values` string outside of the set defined by `Values`.
+          TypeError: `Values` string is not a string type.
+        """
+
+        if not isinstance(values_str, six.string_types):
+            raise TypeError("Values string is not a string type: %s" %
+                            type(values_str))
+
+        try:
+            return self._v2b_dict[values_str]
+        except KeyError:
+            raise ValueError("Values string outside of the set defined by "
+                             "Values: %r" % values_str)
+
+    def items(self):
+        """
+        Generator that iterates through the items of the value mapping. The
+        items are the array entries of the `Values` and `ValueMap` qualifiers,
+        and they are iterated in the order specified in the arrays.
+        If the `ValueMap` qualifier is not specified, the default of consecutive
+        integers starting at 0 is used as a default, consistent with
+        :term:`DSP0004`.
+
+        Each iterated item is a tuple of integer value(s) representing the
+        `ValueMap` array entry, and the corresponding `Values` string.
+        Any integer value in the iterated items is represented as the CIM type
+        of the element (e.g. :class:`~pywbem.Uint16`).
+
+        If the `Values` string corresponds to a single element value, the
+        first tuple item is that single integer value.
+
+        If the `Values` string corresponds to a value range (e.g. "1.." or
+        "..2" or "3..4"), that value range is returned as a tuple with two items
+        that are the lowest and the highest value of the range. That is the
+        case also when the value range is open on the left side or right side.
+
+        If the `Values` string corresponds to the `unclaimed` indicator "..",
+        the first tuple item is `None`.
+
+        Returns:
+
+          :term:`iterator` for tuple of integer value(s) and `Values` string.
+        """
+
+        for values_str in self._v2b_dict:
+            element_value = self._v2b_dict[values_str]
+            yield element_value, values_str
