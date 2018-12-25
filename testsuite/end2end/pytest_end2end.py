1688ecc21af17b904d2dbd392131aa02a132be81
Andreas Maier <andreas.r.maier@gmx.de>
2018-12-25 18:34:38 +0100

    Added profile definitions and more end2end testcases
    Details:
    
    - Added a file profiles.yml that captures discovery-related
      definitions for a number of SNIA and DMTF management profiles.
      The data was copied from the profiledictionary.json file in
      PR #1533, and was updated from the SMI-S standard.
    
    - Added a fixture profile_definition that provides the profile
      definition items found in the profiles.yml file, to test
      cases.
    
    - Added a test case file test_profile_generic.py that uses
      the profile definitions provided by the profile_definition
      fixture and implements some basic tests on central instances
      of the profiles.
    
    - Added a _utils.py module with one function, so far.
    
    Signed-off-by: Andreas Maier <andreas.r.maier@gmx.de>

--- testsuite/end2end/pytest_end2end.py
+++ testsuite/end2end/pytest_end2end.py
@@ -6,15 +6,21 @@ from __future__ import absolute_import
 
 import os
 import pytest
+import yaml
 
 from pywbem import WBEMConnection
 from server_file import ServerDefinitionFile, ServerDefinition
 
-__all__ = ['server_definition', 'default_namespace', 'wbem_connection']
+__all__ = ['server_definition', 'default_namespace', 'wbem_connection',
+           'profile_definition']
 
 # Server nickname or server group nickname in WBEM server definition file
 TESTSERVER = os.getenv('TESTSERVER', 'default')
 SD_LIST = list(ServerDefinitionFile().iter_servers(TESTSERVER))
+
+PROFILES_YAML_FILE = 'profiles.yml'
+with open(PROFILES_YAML_FILE, 'r') as fp:
+    PROFILE_DEFINITIONS = yaml.load(fp)
 
 
 def fixtureid_server_definition(fixture_value):
@@ -94,5 +100,37 @@ def default_namespace(request):
     with.
 
     Returns the default namespace as a string.
+    """
+    return request.param
+
+
+def fixtureid_profile_definition(fixture_value):
+    """
+    Return a fixture ID to be used by py.test, for fixture
+    `profile_definition()`.
+
+    Parameters:
+      * fixture_value (dict): The profile definition dictionary of the profile.
+    """
+    pd = fixture_value
+    assert isinstance(pd, dict)
+    assert 'registered_name' in pd
+    assert 'registered_org' in pd
+    return "profile_definition={0}:{1}". \
+        format(pd['registered_org'], pd['registered_name'])
+
+
+@pytest.fixture(
+    params=PROFILE_DEFINITIONS,
+    scope='module',
+    ids=fixtureid_profile_definition
+)
+def profile_definition(request):
+    """
+    Fixture representing the set of profile definitions to use for the
+    end2end tests.
+
+    Returns the profile definition dictionary of each profile to test against.
+    For dict items, see the description in profiles.yml.
     """
     return request.param
