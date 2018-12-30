1eb5c18e12934cb91775d41862dce520e3dd48d7
Andreas Maier <andreas.r.maier@gmx.de>
2018-12-30 23:00:04 +0100

    End2end testcases for SNIA Server profile; some base support
    Details:
    
    - Added some support in pytest_end2end.py to better support
      profile testcases:
      - Added ProfileTest class as a base class for profile tests.
      - Added single_profile_definition() function that returns
        a single profile definition based on profile org and name.
    
    - Added end2end testcases for SNIA Server profile, as a new file
      test_snia_server_profile.py.
    
    Signed-off-by: Andreas Maier <andreas.r.maier@gmx.de>

--- testsuite/end2end/_utils.py
+++ testsuite/end2end/_utils.py
@@ -4,6 +4,9 @@ Utility functions for end2end tests.
 
 from __future__ import absolute_import, print_function
 
+from pywbem import CIMInstance, CIMInstanceName, \
+    CIMError, CIM_ERR_INVALID_CLASS
+
 
 def _latest_profile_inst(profile_insts):
     """
@@ -20,3 +23,118 @@ def _latest_profile_inst(profile_insts):
             latest_i = i
     latest_profile_inst = profile_insts[latest_i]
     return latest_profile_inst
+
+
+def path_equal(inst1_path, inst2_path):
+    """
+    Return whether two instance paths are equal, with special treatment
+    of their host component:
+    - The host component is compared literally (but case insensitively),
+      i.e. no IP address to hostname translation is performed.
+    - If one or both of the instance paths have their host component set to
+      None, the comparison is considered equal.
+
+    For keys of the instance paths that are references, the special
+    treatment of their host component is applied recursively.
+    """
+    assert isinstance(inst1_path, CIMInstanceName)
+    assert isinstance(inst2_path, CIMInstanceName)
+    if inst1_path.classname.lower() != inst2_path.classname.lower():
+        return False
+    if inst1_path.namespace.lower() != inst2_path.namespace.lower():
+        return False
+    if inst1_path.host is not None and \
+            inst2_path.host is not None and \
+            inst1_path.host.lower() != inst2_path.host.lower():
+        return False
+    if len(inst1_path.keybindings) != len(inst2_path.keybindings):
+        return False
+    for key_name in inst1_path.keybindings:
+        key1_value = inst1_path.keybindings[key_name]
+        if key_name not in inst2_path.keybindings:
+            return False
+        key2_value = inst2_path.keybindings[key_name]
+        if isinstance(key1_value, CIMInstanceName) and \
+                isinstance(key2_value, CIMInstanceName) and \
+                not path_equal(key1_value, key2_value):
+            return False
+        elif key1_value != key2_value:
+            return False
+    return True
+
+
+def path_in(inst_path, inst_path_list):
+    """
+    Return whether an instance path is in an instance path list, with special
+    treatment of their host component when testing for equality, as described
+    for path_equal().
+
+    Each item in inst_path list can be an instance path (CIMInstanceName)
+    or an instance (CIMInstance) whose path is then used for the test.
+    """
+    assert isinstance(inst_path, CIMInstanceName)
+    for ip in inst_path_list:
+        if isinstance(ip, CIMInstance):
+            ip = ip.path
+        assert isinstance(ip, CIMInstanceName)
+        if path_equal(inst_path, ip):
+            return True
+    return False
+
+
+def instance_of(conn, path_list, classname):
+    """
+    Return whether all of a set of CIM instances (identified by their instance
+    paths) are of a particular CIM class, i.e. that the creation class of the
+    CIM instances or one of its superclasses has the specified class name.
+
+    Because there are WBEM servers without support for class operations,
+    this is implemented without relying on class operations. The function
+    performs an EnumerateInstanceNames operation on the desired class in
+    the namespace of the instance in question, and verifies that the
+    instance in question is in the result.
+
+    Parameters:
+
+        path_list (CIMInstanceName or CIMInstance or tuple/list
+          thereof): The CIM instance paths. The instance paths must have a
+          namespace and classname set. The namespace must be the same
+          across all instance paths. The host portion of the instance
+          paths is treated specially when comparing them, as described
+          in path_equal().
+
+        classname (string): The CIM class name.
+    """
+
+    # Check and transform parameters
+    assert path_list is not None
+    if not isinstance(path_list, (tuple, list)):
+        path_list = [path_list]
+    paths = []
+    for obj in path_list:
+        if isinstance(obj, CIMInstance):
+            obj = obj.path
+        assert isinstance(obj, CIMInstanceName)
+        assert obj.namespace is not None
+        assert obj.classname is not None
+        paths.append(obj)
+    del path_list
+    assert len(paths) >= 1
+    namespace = paths[0].namespace
+    for path in paths:
+        assert path.namespace.lower() == namespace.lower()
+
+    try:
+        enum_paths = conn.EnumerateInstanceNames(
+            namespace=namespace, ClassName=classname)
+    except CIMError as exc:
+        if exc.status_code == CIM_ERR_INVALID_CLASS:
+            raise AssertionError(
+                "Class {0!r} does not exist in namespace {1!r}".
+                format(classname, namespace))
+        else:
+            raise
+    for path in paths:
+        if not path_in(path, enum_paths):
+            return False
+    return True
---
+++ testsuite/end2end/test_profile_snia_server.py
@@ -0,0 +1,125 @@
+"""
+End2end tests for SNIA 'Server' profile.
+"""
+
+from __future__ import absolute_import, print_function
+
+# Note: The wbem_connection fixture uses the server_definition fixture, and
+# due to the way py.test searches for fixtures, it also need to be imported.
+from pytest_end2end import wbem_connection, server_definition  # noqa: F401, E501
+
+from pytest_end2end import ProfileTest
+
+
+class Test_SNIA_Server_Profile(ProfileTest):
+    """
+    All end2end tests for SNIA 'Server' profile.
+    """
+
+    def init_profile(self, conn):
+        super(Test_SNIA_Server_Profile, self).init_profile(
+            conn, 'SNIA', 'Server')
+
+    def test_instances(self, wbem_connection):  # noqa: F811
+        """
+        Test the instances of the profile.
+        """
+        self.init_profile(wbem_connection)
+
+        central_inst_paths = self.server.get_central_instances(
+            self.profile_inst.path,
+            central_class=self.profile_definition['central_class'],
+            scoping_class=self.profile_definition['scoping_class'],
+            scoping_path=self.profile_definition['scoping_path'],
+            reference_direction=self.profile_definition['reference_direction'])
+
+        # Check that there is one central instance
+        assert len(central_inst_paths) == 1
+        central_inst_path = central_inst_paths[0]
+
+        # Test the CIM_ObjectManager central instance
+        self.assert_instance_of(central_inst_path, 'CIM_ObjectManager')
+        central_inst = self.conn.GetInstance(central_inst_path)
+        self.assert_instance_of(central_inst, 'CIM_ObjectManager')
+        self.assert_instance_consistency(central_inst, central_inst_path)
+        mandatory_property_list = [
+            'SystemCreationClassName', 'SystemName',
+            'CreationClassName', 'Name',
+            'ElementName', 'Description',
+            'OperationalStatus', 'Started',
+        ]
+        self.assert_mandatory_properties(central_inst, mandatory_property_list)
+
+        # Test the associated CIM_Namespace instances
+        far_insts, _ = self.assert_association(
+            source_path=central_inst_path,
+            source_role='Antecedent',
+            assoc_class='CIM_NamespaceInManager',
+            far_role='Dependent',
+            far_class='CIM_Namespace')
+        mandatory_property_list = [
+            'SystemCreationClassName', 'SystemName',
+            'ObjectManagerCreationClassName', 'ObjectManagerName',
+            'CreationClassName', 'Name',
+            'ClassType',
+        ]
+        for inst in far_insts:
+            self.assert_mandatory_properties(inst, mandatory_property_list)
+
+        # Check that they are the namespaces determined by WBEMServer
+        inst_names = [inst['Name'].lower() for inst in far_insts]
+        determined_names = [ns.lower() for ns in self.server.namespaces]
+        assert set(inst_names) == set(determined_names)
+
+        # Test the associated CIM_ObjectManagerCommunicationMechanism instances
+        far_insts, _ = self.assert_association(
+            source_path=central_inst_path,
+            source_role='Antecedent',
+            assoc_class='CIM_CommMechanismForManager',
+            far_role='Dependent',
+            far_class='CIM_ObjectManagerCommunicationMechanism')
+        mandatory_property_list = [
+            'SystemCreationClassName', 'SystemName',
+            'CreationClassName', 'Name',
+            'ElementName', 'CommunicationMechanism',
+            'FunctionalProfilesSupported',
+            'MultipleOperationsSupported',
+            'AuthenticationMechanismsSupported',
+        ]
+        for inst in far_insts:
+            self.assert_mandatory_properties(inst, mandatory_property_list)
+            self.assert_property_one_of(
+                inst, 'CommunicationMechanism', [2, 4])
+            # TODO 2018-12 AM: The SNIA Server profile mandates that the
+            #      FunctionalProfilesSupported property is 0 (Unknown), but it
+            #      is an array property.
+            # TODO 2018-12 AM: OP returns a set of functional profiles, which
+            #      is in conflict with the profile. However, probably most
+            #      WBEM servers will do so.
+            # self.assert_property_contains(
+            #     inst, 'FunctionalProfilesSupported', 0)
+            self.assert_property_one_of(
+                inst, 'MultipleOperationsSupported', [False])
+
+        # Check that there is at least one associated CIM_ObjectManagerComm...
+        assert len(far_insts) >= 1
+
+        # Test the associated CIM_System instances
+        far_insts, _ = self.assert_association(
+            source_path=central_inst_path,
+            source_role='Dependent',
+            assoc_class='CIM_HostedService',
+            far_role='Antecedent',
+            far_class='CIM_System')
+        mandatory_property_list = [
+            'CreationClassName', 'Name',
+            'NameFormat', 'Description', 'ElementName', 'OperationalStatus',
+        ]
+        for inst in far_insts:
+            self.assert_mandatory_properties(inst, mandatory_property_list)
+            self.assert_property_one_of(
+                inst, 'NameFormat', ['IP', 'WWN', 'Other'])
+
+        # Check that there is one associated CIM_System instance
+        assert len(far_insts) == 1
+        # system_inst = far_insts[0]
