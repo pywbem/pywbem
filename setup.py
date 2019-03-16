e9ecf05ced1d27737575e7ebcea9d421a962dfc7
Andreas Maier <maiera@de.ibm.com>
2019-03-16 13:18:01 +0100

    Improved Python version checking by using 'python_requires' keyword
    Details:
    
    - So far, pywbem performs a check for the supported Python versions
      when the pywbem module is loaded (in pywbem/__init__.py), plus it
      specifies the supported Python versions in the setup metadata using
      the "Programming Language" classifiers. The latter causes for
      examiple the supported Python versions to be shown on Pypi, but
      these classifiers are not sufficient for pip to perform a Python
      version check during installation. Which is why the pywbem runtime
      check was added in the first place.
    
      As a result, pip does install pywbem on unsupported Python versions,
      and only at the time the module is loaded, does the pywbem check
      occur.
    
      In addition, this prevents the benefit of the pip feature of
      automatically finding the latest version of a package that supports
      a specific Python version.
    
      This change improves the Python version check by using the
      "python_requires" keyword, thus making pip aware of the
      supported Python versions. There are two ways to specify this
      keyword: In setup.cfg (requires setuptools>=34.0.0) and in
      setup.py (work with our current minimum setuptools=33.1.1).
      In order not to increase the minimumsetuptools version, the
      keyword is specified in setup.py, along with an according
      comment explaining why.
    
    - Because installation via `python setup.py install` does not honor
      the python_requires keyword, the runtime check for the Python
      version in pywbem/__init__.py still remains.
    
    Signed-off-by: Andreas Maier <maiera@de.ibm.com>

--- setup.py
+++ setup.py
@@ -26,4 +26,13 @@ import setuptools
 
 setuptools.setup(
     setup_requires=['pbr>=1.10'],
+
+    # Specifying python_requires in setup.cfg would be more logical
+    # but requires setuptools>=34.0.0, hence it is specified here.
+    # This works with the current minimum versions of
+    # setuptools=33.1.1 and pip=9.0.1.
+    # !!! Make sure to keep the supported Python versions in sync
+    # between setup.py, setup.cfg and pywbem/__init__.py !!!
+    python_requires='>=2.6, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
+
     pbr=True)
