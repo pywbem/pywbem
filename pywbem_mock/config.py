cea5119709b891c761c4c9b54ee5999af95549b8
kschopmeyer <k.schopmeyer@swbell.net>
2020-06-08 10:48:40 -0500

    Fixes issue # 2065 - Instance iq and ico variations
    Adds config variables to define if the instance request
    IncludeQualifiers and IncludeClassOrigin are honored in the pywbem_mock
    GetInstance and EnumerateInstance response generators since these
    request parameters have been deprecated in DMTF 0200 with the proposed
    solution being to not return either qualifiers or class origin.

--- pywbem_mock/config.py
+++ pywbem_mock/config.py
@@ -34,7 +34,8 @@ modify the configuration variables.
 
 __all__ = ['OBJECTMANAGERNAME', 'SYSTEMNAME',
            'SYSTEMCREATIONCLASSNAME', 'DEFAULT_MAX_OBJECT_COUNT',
-           'OPEN_MAX_TIMEOUT', 'OBJECTMANAGERCREATIONCLASSNAME']
+           'OPEN_MAX_TIMEOUT', 'OBJECTMANAGERCREATIONCLASSNAME',
+           'IGNORE_INSTANCE_IQ_PARAM', 'IGNORE_INSTANCE_ICO_PARAM']
 
 #: Name for the object manager It is used in defining user-defined provider
 #: properties for CIM_Namespace provider and CIM_ObjectManager provider if
@@ -60,3 +61,38 @@ DEFAULT_MAX_OBJECT_COUNT = 100
 #: Maximum timeout in seconds for pull operations if the OperationTimeout
 #: parameter is not included in the request
 OPEN_MAX_TIMEOUT = 40
+
+#: Use of the IncludeQualifiers parameter on instance requests is DEPRECATED in
+#: DMTF DSP0200. The definition of IncludeQualifiers is ambiguous and when this
+#: parameter is set to true, WBEM clients cannot be assured that any qualifiers
+#: will be returned. A WBEM client should always set this parameter to false. To
+#: minimize the impact of this recommendation on WBEM clients, a WBEM server may
+#: choose to treat the value of IncludeQualifiers as false for all requests.
+#:
+#: The following variable forces pywbem_mock to ignore the client supplied
+#: variable and not return qualifiers on EnumerateInstances and GetInstance
+#: responses.
+#:
+#: * True (default): pywbem_mock always removes qualifiers from instances
+#:   in responses
+#: * False: pywbem_mock uses value of input parameter or its default to
+#:   determine if qualifiers are to be removed
+IGNORE_INSTANCE_IQ_PARAM = True
+
+#: Use of the IncludeClassOrigin parameter on instance requests is DEPRECATED. A
+#: WBEM server may choose to treat the value of IncludeClassOrigin parameter as
+#: false for all requests, otherwise the implementation shall support the
+#: original behavior as defined in the rest of this paragraph. If the
+#: IncludeClassOrigin input parameter is true, the CLASSORIGIN attribute shall
+#: be present on all appropriate elements in each returned Instance. If it is
+#: false, no CLASSORIGIN attributes are present.
+#:
+#: The following variable forces pywbem_mock to ignore the client supplied
+#: variable and not return qualifiers on EnumerateInstances and GetInstance
+#: responses.
+#:
+#: * True (default): pywbem_mock always removes class origin attributes from
+#:   instances in responses
+#: * False: pywbem_mock uses value of input parameter or its default to
+#:   determine if class origin attributes are to be removed
+IGNORE_INSTANCE_ICO_PARAM = True
