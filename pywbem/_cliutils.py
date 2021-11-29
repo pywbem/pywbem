ee5febef133d54a69f135d0125404e1f36758254
Andreas Maier <andreas.r.maier@gmx.de>
2021-11-29 07:46:27 +0100

    Fixed new issues raised by Pylint 2.12.1
    Signed-off-by: Andreas Maier <andreas.r.maier@gmx.de>

--- pywbem/_cliutils.py
+++ pywbem/_cliutils.py
@@ -47,4 +47,5 @@ class SmartFormatter(argparse.HelpFormatter):
     def _split_lines(self, text, width):
         if text.startswith('R|'):
             return text[2:].splitlines()
+        # pylint: disable=protected-access
         return argparse.HelpFormatter._split_lines(self, text, width)
