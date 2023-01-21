8d87f1115c07e900b24eac5f03bbf780e2e28da7
Andreas Maier <andreas.r.maier@gmx.de>
2023-01-21 04:31:18 +0100

    Start 1.6.1
    Signed-off-by: Andreas Maier <andreas.r.maier@gmx.de>

--- pywbem/_version.py
+++ pywbem/_version.py
@@ -29,4 +29,4 @@ Version of the pywbem package.
 #:
 #: * "M.N.P.devD": Development level D of a not yet released version M.N.P
 #: * "M.N.P": A released version M.N.P
-__version__ = '1.6.0'
+__version__ = '1.6.1.dev1'
