281e817ee13e175ffaebdbaff8a9c170d496affb
Karl Schopmeyer <k.schopmeyer@swbell.net>
2023-11-05 18:06:24 -0600

    Fixes issue #3071 end end test fail. (#3076)
    * Fix issue in end2end/test_indications
    
    Fix issue documented in issue #3071.  This was a code error. There was a
    case where the time for the last indication received variable was not
    set correctly.
    
    This also adds the last sequence number to the error and changes  from a
    display of the issue to an assert. The test should fail if the
    indications were not received as a monotonically increasing sequence
    with no gaps in the sequence.
    
    * Fix issue #3071, end2end indication test failure.
    
    Corrected code issue where the last indication time was not always
    captured before testing for elapsed time.
    
    Also modified the code testing for next sequence number to assert if it
    is not correct since that is actually an error.
    
    Not in changes since this is a change within a single pywbem
    version
    
    ---------
    
    Co-authored-by: kschopmeyer <k.schopmeyerwork@gmail.com>

--- tests/end2endtest/test_indications.py
+++ tests/end2endtest/test_indications.py
@@ -360,14 +360,16 @@ class RunIndicationTest(object):  # pylint: disable=too-many-instance-attributes
             self.last_seq_num = seq_num
             self.received_indication_count = 1
         else:
-            if seq_num != (self.last_seq_num + 1):
-                print('Missed {0} indications at {1}'.
-                      format(((seq_num - 1) - self.last_seq_num),
-                             self.last_seq_num))
-
-            self.last_seq_num = seq_num
-            self.last_indication_time = datetime.datetime.now()
+            exp_seq_num = self.last_seq_num + 1
+            assert seq_num == exp_seq_num, \
+                'Missed {0} indications at seq_num={1}. rcvd seq num={2}, ' \
+                'expected={3}'. \
+                format((seq_num - 1) - self.last_seq_num,
+                       self.last_seq_num, seq_num, exp_seq_num)
             self.received_indication_count += 1
+
+        self.last_seq_num = seq_num
+        self.last_indication_time = datetime.datetime.now()
 
         # pylint: disable=logging-format-interpolation
         self.listener.logger.info(
