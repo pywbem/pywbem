5490e50cdf8f4e59e96ed2f6d51d9857e6de13d4
Andreas Maier <andreas.r.maier@gmx.de>
2024-05-21 13:21:49 +0200

    Fixed new issues reported by Pylint 3.2
    Signed-off-by: Andreas Maier <andreas.r.maier@gmx.de>

--- pywbem/_mof_compiler.py
+++ pywbem/_mof_compiler.py
@@ -1854,6 +1854,7 @@ def p_instanceDeclaration(p):
             # qualifiers, default value,
             pprop = cprop.copy()
             embedded_object_type = None
+            allowed_types = None
             pprop.qualifiers = NocaseDict()
             # If embedded instance/object property, compile the value component
             # and put into the property value.
