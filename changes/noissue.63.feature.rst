5286abb10e49373bc0fa01ec62a3c8c1eb673727
Andreas Maier <andreas.r.maier@gmx.de>
2025-10-26 02:43:07 +0100

    Improved the 'examples/listen.py' script
    Details:
    
    * Improved argument parsing: It now uses argparse and specifies only the
      listener host as a positional parameter and everything else as options.
    
    * It now can set the maximum indication delivery queue size via a new option
      --max-queue-size.
    
    * The indication processing callback now can sleep for some time to test queue
      full behavior, via a new option --sleep. It can also be set in the Python
      console via the 'sleep' variable.
    
    * Added stopping the listener when exiting the console, to clean it up and
      this is required to not hang on exit.
    
    * Removed unnecessary global statements.
    
    * Removed duplicate showing of counters when resetting them.
    
    * Renamed status() to counters() to better match what it does.
    
    * Separated printing of parameters from Python console banner.
    
    * Changed use of log in process_indication() to use print, so that any logs
      are always WBEMListener logs.
    
    * Moved instructions how to create certfile and keyfile to the help epilog.
    
    * Removed examples from the help epilog, because that is not necessary with
      the help.
    
    Signed-off-by: Andreas Maier <andreas.r.maier@gmx.de>

---
+++ changes/noissue.63.feature.rst
@@ -0,0 +1,4 @@
+Examples: Improved the ``examples/listen.py`` script: Improved argument parsing,
+it now can set the maximum indication delivery queue size, the indication
+processing callback now can sleep for some time to test queue full behavior,
+stopped the listener when exiting the console.
--- examples/listen.py
+++ examples/listen.py
@@ -1,159 +1,234 @@
 #!/usr/bin/env python
 
-''' Test listener. When started this code creates a WBEMListener on the
-    address/ports defined by the input parameters.  This listener
-    displays any indications received as log entries.
-'''
+"""
+Test listener. When started this code creates a WBEMListener on the
+address/ports defined by the input parameters.  This listener displays any
+indications received.
+"""
 
-import sys as _sys
-import os as _os
-import code as _code
-import errno as _errno
+import sys
+import os
+import time
+import code
 import logging
 import threading
-# Conditional support of readline module
+import argparse
+# Conditional support of readline module - it is not available on some platforms
 try:
-    import readline as _readline
-    _HAVE_READLINE = True
+    import readline
 except ImportError:
-    _HAVE_READLINE = False
+    readline = None
 
 import pywbem
-
-# For creating a self-signed certificate file with private key inside, issue:
-#    openssl req -new -x509 -keyout server.pem -out server.pem -days 365 -nodes
 
 # Dictionary to count indications received by host
 RECEIVED_INDICATION_DICT = {}
 COUNTER_LOCK = threading.Lock()
 
-#pylint: disable=invalid-name
-listener = None
+# Other globals
+listener = None  # pylint: disable=invalid-name
+sleep = None  # pylint: disable=invalid-name
 
-def _process_indication(indication, host):
-    '''This function gets called when an indication is received.'''
+# Defaults for the command line
+DEFAULT_HOST = "localhost"
+DEFAULT_HTTP_PORT = 5000
+DEFAULT_HTTPS_PORT = None
+DEFAULT_SLEEP = 0
+DEFAULT_MAX_QUEUE_SIZE = 5
 
-    global RECEIVED_INDICATION_DICT
-    COUNTER_LOCK.acquire()
-    if host in RECEIVED_INDICATION_DICT:
-        RECEIVED_INDICATION_DICT[host] += 1
-    else:
-        RECEIVED_INDICATION_DICT[host] = 1
-    COUNTER_LOCK.release()
-
-    listener.logger.info("Consumed CIM indication #%s: host=%s\n%s",
-                         RECEIVED_INDICATION_DICT, host, indication.tomof())
-
-def _get_argv(index, default=None):
-    ''' get the argv input argument defined by index. Return the default
-        attribute if that argument does not exist
-    '''
-    return _sys.argv[index] if len(_sys.argv) > index else default
+DEFAULT_LOG_LEVEL = logging.INFO
 
 
-def status(reset=None):
-    '''
-        Show status of indications received. If optional reset attribute
-        is True, reset the counter.
-    '''
-    global RECEIVED_INDICATION_DICT
+def parse_args(prog):
+    """
+    Create an argument parser and parse the command line arguments.
+    """
+
+    usage = "%(prog)s [options] [HOST]"
+    desc = ("Start a WBEM listener and provide an interactive Python console "
+            "for issuing operations against it.")
+    epilog = ("Note: For creating a self-signed certificate file with private "
+              "key inside, issue:\n"
+              "  openssl req -new -x509 -keyout server.pem -out server.pem "
+              "-days 365 -nodes")
+
+    parser = argparse.ArgumentParser(
+        prog=prog, usage=usage, description=desc, epilog=epilog, add_help=True,
+        formatter_class=argparse.RawTextHelpFormatter)
+
+    parser.add_argument(
+        "host", metavar="HOST", nargs='?', default=DEFAULT_HOST,
+        help="The host for which indications are accepted by the listener:\n"
+        "  - a specific host name or host IP address on the local computer:\n"
+        "    only indications addressed to that host will be accepted.\n"
+        "  - a wildcard address (0.0.0.0 (IPv4) or :: (IPv6))) or the empty"
+        " string:\n"
+        "    the listener will accept indications addressed to any network"
+        " address.\n"
+        f"Default: {DEFAULT_HOST}")
+
+    parser.add_argument(
+        "--http-port", type=int, default=DEFAULT_HTTP_PORT,
+        help="Enables the listener thread for HTTP on this port. "
+        f"Default: {DEFAULT_HTTP_PORT}")
+
+    parser.add_argument(
+        "--https-port", type=int, default=DEFAULT_HTTPS_PORT,
+        help="Enables the listener thread for HTTPS on this port. "
+        f"Default: {DEFAULT_HTTPS_PORT}")
+
+    parser.add_argument(
+        "--max-queue-size", type=int, default=DEFAULT_MAX_QUEUE_SIZE,
+        help="Maximum size of the indication delivery queue of the listener. "
+        f"Default: {DEFAULT_MAX_QUEUE_SIZE}")
+
+    parser.add_argument(
+        "--certfile", type=str, default=None,
+        help="Certificate file for HTTPS. Required, if HTTPS is used.")
+
+    parser.add_argument(
+        "--keyfile", type=str, default=None,
+        help="Key file for HTTPS. Required, if HTTPS is used.")
+
+    parser.add_argument(
+        "--sleep", type=int, default=DEFAULT_SLEEP,
+        help="Sleep time in seconds when receiving an indication. "
+        f"Default: {DEFAULT_SLEEP}")
+
+    args = parser.parse_args()
+    return args
+
+
+def process_indication(indication, host):
+    """
+    This function gets called when an indication is received.
+    """
+    with COUNTER_LOCK:
+        if host in RECEIVED_INDICATION_DICT:
+            RECEIVED_INDICATION_DICT[host] += 1
+        else:
+            RECEIVED_INDICATION_DICT[host] = 1
+
+    print("process_indication: Processing CIM indication "
+          f"(counters: {RECEIVED_INDICATION_DICT}):\n"
+          f"host={host}\n"
+          f"{indication.tomof()}")
+    if sleep > 0:
+        print(f"process_indication: Sleeping for {sleep} s")
+        time.sleep(sleep)
+
+
+def counters(reset=None):
+    """
+    Show counters of indications received.
+    If reset is True, reset the counters.
+    """
     for host, count in RECEIVED_INDICATION_DICT.items():
-        print(f'Host {host} Received {count} indications')
+        print(f"Host {host} received {count} indications")
 
     if reset:
         for host in RECEIVED_INDICATION_DICT:
             RECEIVED_INDICATION_DICT[host] = 0
-            print(f'Host {host} Reset: Received '
-                  f'{RECEIVED_INDICATION_DICT[host]} indications')
-        print('counts reset to 0')
+        print("Counters have been reset to 0")
 
 
-def _main():
-    ''' Main function gets input parameters, sets up listener and starts
-        the listener
-    '''
-    global listener
+def main():
+    """
+    Main function gets input parameters, sets up listener and starts the
+    listener.
+    """
+    global listener  # pylint: disable=global-statement
+    global sleep  # pylint: disable=global-statement
 
-    if len(_sys.argv) < 2 or _sys.argv[1] == '--help':
-        print(f"Usage: {_sys.argv[0]} host http_port "
-              "[https_port certfile keyfile]")
-        _sys.exit(2)
+    args = parse_args(sys.argv[0])
 
-    logging.basicConfig(stream=_sys.stderr, level=logging.INFO,
-                        format='%(asctime)s %(levelname)s: %(message)s')
+    log_level = DEFAULT_LOG_LEVEL
+    logging.basicConfig(stream=sys.stderr, level=log_level,
+                        format="%(asctime)s %(levelname)s: %(message)s")
 
-    host = _get_argv(1)
-    http_port = _get_argv(2)
-    https_port = _get_argv(3)
-    certfile = _get_argv(4)
-    keyfile = _get_argv(5)
+    host = args.host
+    http_port = args.http_port
+    https_port = args.https_port
 
+    # These parameters are intentionally not checked in order to be able to
+    # test how WBEMListener handles them when missing.
+    certfile = args.certfile
+    keyfile = args.keyfile
+
+    max_queue_size = args.max_queue_size
+    sleep = args.sleep
+
+    print("WBEMListener parameters:")
+    print(f"  host={host}")
+    print(f"  http_port={http_port}")
+    print(f"  https_port={https_port}")
+    print(f"  certfile={certfile}")
+    print(f"  keyfile={keyfile}")
+    print(f"  max_ind_queue_size={max_queue_size}")
+    print(f"Sleep time when processing indications: {sleep} s")
+    print(f"Log level (to stderr): {logging.getLevelName(log_level)}")
+    print("")
+
+    print("Creating WBEMListener instance.")
     listener = pywbem.WBEMListener(host=host,
                                    http_port=http_port,
                                    https_port=https_port,
                                    certfile=certfile,
-                                   keyfile=keyfile)
+                                   keyfile=keyfile,
+                                   max_ind_queue_size=max_queue_size)
 
-    listener.add_callback(_process_indication)
+    print("Adding callback: process_indication")
+    listener.add_callback(process_indication)
+
+    print("Starting WBEM listener.")
     try:
         listener.start()
     except pywbem.ListenerError as exc:
         print(f"Error: {exc}")
         return 1
 
-    banner = f"""
-WBEM listener started on host {host} (HTTP port: {http_port}, HTTPS port: {https_port}).
-The host parameter may be:
-
-  * a specific host name or host IP address on the computer; only
-    indications addressed to that host will be accepted:
-  * a wildcard address (0.0.0.0 (IPV4) or ;; (IPV6))) - the listener will accept
-    indications addressed to any network address on the host system.
-  * The empty string ( "" ) - the listener will accept indications addressed to
-    any network address on the host system.
-
-This Python console displays any indications received by this listener as
-logger outputs (Level=INFO) to the console by default.
-
-listener: WBEMListener instance that has been started.
-
-help(listener): Display help for using listener instance.
-
-status() : Display count of indications received; status(reset=True) to reset
-           counters
-
-Modify logger characteristics through listener.logger:
-     (ex. listener.logger.setLevel(logging.ERROR))
-
-Ctrl-d to exit
-"""
-
     # Determine file path of history file
     home_dir = '.'
-    if 'HOME' in _os.environ:
-        home_dir = _os.environ['HOME'] # Linux
-    elif 'HOMEPATH' in _os.environ:
-        home_dir = _os.environ['HOMEPATH'] # Windows
+    if 'HOME' in os.environ:
+        home_dir = os.environ['HOME']  # Linux
+    elif 'HOMEPATH' in os.environ:
+        home_dir = os.environ['HOMEPATH']  # Windows
     histfile = f'{home_dir}/.listen_history'
 
     # Read previous command line history
-    if _HAVE_READLINE:
-        NotFoundError = getattr(__builtins__, 'FileNotFoundError', IOError)
+    if readline:
         try:
-            _readline.read_history_file(histfile)
-        except NotFoundError as exc:
-            if exc.errno != _errno.ENOENT:
-                raise
+            readline.read_history_file(histfile)
+        except FileNotFoundError:
+            pass
 
-    # Interact
-    i = _code.InteractiveConsole(globals())
+    # Start Python console and interact
+    banner = """
+This Python console displays any indications received by this listener.
+
+Some variables and functions:
+
+* listener: WBEMListener instance that has been started.
+* help(listener): Display help for using listener instance.
+* sleep: Sleep time in seconds when receiving an indication (can be set).
+* counters(): Display indication counters.
+* counters(reset=True): Display and reset indication counters.
+* listener.logger.setLevel(logging.DEBUG): Change log level.
+
+Ctrl-D: Exit this console.
+"""
+    i = code.InteractiveConsole(globals())
     i.interact(banner)
 
     # Save command line history
-    if _HAVE_READLINE:
-        _readline.write_history_file(histfile)
+    if readline:
+        readline.write_history_file(histfile)
+
+    print("Stopping listener")
+    listener.stop()
 
     return 0
 
+
 if __name__ == '__main__':
-    _sys.exit(_main())
+    sys.exit(main())
